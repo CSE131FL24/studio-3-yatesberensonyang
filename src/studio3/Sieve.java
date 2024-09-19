0debb9ccee8c3c225d101c4e2be70b33025aa934
TheoB <TheoB@localhost>
2024-09-19 11:10:31 -0500

    I tried

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,21 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Set n: ");
+		int n = scan.nextInt();
+		
+		int[ ] array = new int [n];
+		for(int i = 0; i < array.length; i++) {
+			System.out.println( array[i] );
+		
+	}
+
+}
+
+}
